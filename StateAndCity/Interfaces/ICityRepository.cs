159f117468d5470c9f7cd24d2f7899a746073056
masuodfathi <Masuod.fathi@gmail.com>
2024-01-12 16:44:42 +0330

    Interfaces

---
+++ StateAndCity/Interfaces/ICityRepository.cs
@@ -0,0 +1,19 @@
+﻿using StateAndCity.Models;
+
+namespace StateAndCity.Interfaces
+{
+    public interface ICityRepository
+    {
+        ICollection<CityModel> GetCities();
+        CityModel GetCityById(int id);
+        CityModel GetCityByName(string name);
+        bool CreateCity(CityModel city);
+        bool UpdateCity(CityModel city);
+        bool DeleteCity(int id);
+
+        // methods for image operations
+        bool SaveCityImagePath(int cityId, string imagePath);
+        string GetCityImageUrl(int cityId);
+
+    }
+}
---
+++ StateAndCity/Interfaces/ICityService.cs
@@ -0,0 +1,18 @@
+﻿using StateAndCity.Models;
+
+namespace StateAndCity.Interfaces
+{
+    public interface ICityService
+    {
+        ICollection<CityModel> GetCities();
+        CityModel GetCityById(int id);
+        CityModel GetCityByName(string name);
+        bool CreateCity(CityModel city);
+        bool UpdateCity(CityModel city);
+        bool DeleteCity(int id);
+
+        // Image-related methods
+        string GetCityImagePath(int cityId);
+        bool SaveCityImagePath(int cityId, string imagePath);
+    }
+}
