c74d50d0610e5e8c7cdb14bbe891355804bf2061
masuodfathi <Masuod.fathi@gmail.com>
2024-01-12 16:45:07 +0330

    Services

---
+++ StateAndCity/Services/CityService.cs
@@ -0,0 +1,54 @@
+﻿using StateAndCity.Interfaces;
+using StateAndCity.Models;
+
+namespace StateAndCity.Services
+{
+    public class CityService : ICityService
+    {
+        private readonly ICityRepository _cityRepository;
+
+        public CityService(ICityRepository cityRepository)
+        {
+            _cityRepository = cityRepository;
+        }
+
+        public CityModel GetCityById(int id)
+        {
+            return _cityRepository.GetCityById(id);
+        }
+
+        public ICollection<CityModel> GetCities()
+        {
+            return _cityRepository.GetCities();
+        }
+        public string GetCityImagePath(int cityId)
+        {
+            return _cityRepository.GetCityImageUrl(cityId);
+        }
+
+        public CityModel GetCityByName(string name)
+        {
+            return _cityRepository.GetCityByName(name);
+        }
+
+        public bool CreateCity(CityModel city)
+        {
+            return _cityRepository.CreateCity(city);
+        }
+
+        public bool UpdateCity(CityModel city)
+        {
+            return _cityRepository.UpdateCity(city);
+        }
+
+        public bool DeleteCity(int id)
+        {
+            return _cityRepository.DeleteCity(id);
+        }
+
+        public bool SaveCityImagePath(int cityId, string imagePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StateAndCity/Services/StateService.cs
@@ -0,0 +1,40 @@
+﻿using StateAndCity.Interfaces;
+using StateAndCity.Models;
+
+namespace StateAndCity.Services
+{
+    public class StateService : IStateService
+    {
+        private readonly IStateRepository _stateRepository;
+
+        public StateService(IStateRepository stateRepository)
+        {
+            _stateRepository = stateRepository;
+        }
+
+        public ICollection<StateModel> GetStates()
+        {
+            return _stateRepository.GetStates();
+        }
+
+        public StateModel GetState(int id)
+        {
+            return _stateRepository.GetState(id);
+        }
+
+        public StateModel GetState(string stateName)
+        {
+            return _stateRepository.GetState(stateName);
+        }
+
+        public ICollection<CityModel> GetCitiesByState(string stateName)
+        {
+            return _stateRepository.GetCitiesByState(stateName);
+        }
+
+        public ICollection<CityModel> GetCitiesByState(int id)
+        {
+            return _stateRepository.GetCitiesByState(id);
+        }
+    }
+}
