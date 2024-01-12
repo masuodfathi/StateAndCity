4df0270830a5a90bc9748a0b7f282895e26f2b02
masuodfathi <Masuod.fathi@gmail.com>
2024-01-13 00:33:19 +0330

    seperate js file from select location view

--- StateAndCity/wwwroot/js/site.js
+++ StateAndCity/wwwroot/js/site.js
@@ -1,4 +1,54 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('#state').change(function () {
+        var selectedStateId = $(this).val();
 
-// Write your JavaScript code.
+        // Reset city dropdown
+        $('#city').val('');
+        $('#city').prop('disabled', true);
+        $('#cityImageContainer').hide();
+
+        if (selectedStateId !== '') {
+            // Fetch cities for the selected state using AJAX
+            $.ajax({
+                type: 'GET',
+                url: '/SelectLocation/GetCitiesByState',
+                data: { stateId: selectedStateId },
+                success: function (data) {
+                    // Update city dropdown
+                    $('#city').empty();
+                    $('#city').append('<option value="">-- Select City --</option>');
+
+                    $.each(data, function (index, city) {
+                        $('#city').append('<option value="' + city.cityId + '">' + city.cityName + '</option>');
+                    });
+
+                    $('#city').prop('disabled', false);
+                    
+                }
+            });
+        }
+    });
+
+    $('#city').change(function () {
+        var cityImage = $('#cityImage');
+        var selectedCityId = $(this).val();
+        var cityImageContainer = $('#cityImageContainer');
+        cityImageContainer.hide();
+        if (selectedCityId !== '') {
+            // Fetch city image path using AJAX
+            $.ajax({
+                type: 'GET',
+                url: '/SelectLocation/GetCityImage',
+                data: { cityId: selectedCityId },
+                success: function (data) {
+                    // Display city image
+                    cityImage.attr('src', data);
+                    cityImageContainer.show();
+                }
+            });
+        } else {
+            // Hide city image container if no city selected
+            $('#cityImageContainer').hide();
+        }
+    });
+});
