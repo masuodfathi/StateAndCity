f64cdec2f835da1d6fb98cb6f3ed04c59e27e409
masuodfathi <Masuod.fathi@gmail.com>
2024-01-12 16:44:51 +0330

    Models

--- StateAndCity/Models/CityModel.cs
+++ StateAndCity/Models/CityModel.cs
@@ -4,7 +4,7 @@
     {
         public int CityId { get; set; }
         public string CityName { get; set; }
-
+        public string ImageUrl {  get; set; }
 
         public int StateId { get; set; }
         public StateModel State { get; set; }
