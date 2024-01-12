c36a156b1f0c6e1bdf3aa35abb245bee5f4b8c34
masuodfathi <Masuod.fathi@gmail.com>
2024-01-12 16:45:17 +0330

    ViewModels

---
+++ StateAndCity/ViewModels/CityViewModel.cs
@@ -0,0 +1,12 @@
+﻿using StateAndCity.Models;
+
+namespace StateAndCity.ViewModels
+{
+    public class CityViewModel
+    {
+        public int SelectedStateId { get; set; }
+        public int SelectedCityId { get; set; }
+        public List<StateModel> States { get; set; }
+        public List<CityModel> Cities { get; set; }
+    }
+}
