128d7e2f0423f7a2eeb67ed6e86959bdc13091a6
masuodfathi <Masuod.fathi@gmail.com>
2024-01-12 16:44:32 +0330

    DBContext

--- StateAndCity/Data/DataContext.cs
+++ StateAndCity/Data/DataContext.cs
@@ -11,7 +11,35 @@ namespace StateAndCity.Data
         }
         public DbSet<StateModel> States { get; set; }
         public DbSet<CityModel> Cities { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CityModel>()
+                .HasKey(c => c.CityId);
+
+            // Define primary key for CityModel
+        modelBuilder.Entity<CityModel>()
+            .HasKey(c => c.CityId);
+
+        // Define foreign key relationship between CityModel and StateModel
+        modelBuilder.Entity<CityModel>()
+            .HasOne(c => c.State)
+            .WithMany(s => s.Cities)
+            .HasForeignKey(c => c.StateId)
+            .OnDelete(DeleteBehavior.Cascade); // Choose appropriate delete behavior
+
+        // Define primary key for StateModel
+        modelBuilder.Entity<StateModel>()
+            .HasKey(s => s.StateId);
 
         
+
+        // Example: Adding unique constraint on StateName in StateModel
+        modelBuilder.Entity<StateModel>()
+            .HasIndex(s => s.StateName)
+            .IsUnique();
+        }
+
     }
 }
